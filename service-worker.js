a7089a9a50eac26ab3702e5af4c35dc75d93a921
Ivan.Trushin <rymbln@gmail.com>
2026-02-08 12:15:15 +0300

    init

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "umforum8-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+
+  "./style/main.css",
+
+  "./js/bind_polyfill.js",
+  "./js/classlist_polyfill.js",
+  "./js/animframe_polyfill.js",
+  "./js/keyboard_input_manager.js",
+  "./js/html_actuator.js",
+  "./js/grid.js",
+  "./js/tile.js",
+  "./js/local_storage_manager.js",
+  "./js/game_manager.js",
+  "./js/application.js",
+
+  "./favicon.ico",
+
+  // если используешь картинки плиток
+  "./assets/img/64.png",
+  "./assets/img/128.png",
+  "./assets/img/256.png",
+  "./assets/img/512.png"
+];
+
+// Установка — кэшируем всё
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Активация
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Перехват запросов (offline-first)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
